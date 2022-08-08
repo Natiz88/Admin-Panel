786caa1a37a84e05bdce8455ca8c963320431595
Natiz Bista <bistanatiz10@gmail.com>
2022-08-08 12:53:34 +0545

    banners

--- src/pages/Banners.js
+++ src/pages/Banners.js
@@ -1,26 +1,73 @@
 import React from "react";
+import { ImCross } from "react-icons/im";
+import PageTitle from "../components/Typography/PageTitle";
 import { Link } from "react-router-dom";
+import Images from "./../utils/images/Images";
+import { useState } from "react";
+// import photo from "./imagesbanner.jpg";
+
 import {
-  Input,
-  Form,
-  HelperText,
+  Modal,
   Label,
   Button,
-  Select,
+  Input,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
   Textarea,
 } from "@windmill/react-ui";
 
-import PageTitle from "../components/Typography/PageTitle";
-
 const Banners = () => {
+  console.log("images from", Images);
+
+  const [img, setImg] = useState(null);
+  const onImageChange = (e) => {
+    const [file] = e.target.files;
+    setImg(URL.createObjectURL(file));
+  };
+
   return (
-    <div>
+    <>
       <PageTitle>Banners</PageTitle>
-      <Button tag={Link} to="/app/newBanner">
-        Add new banner
-      </Button>
-      <form className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800"></form>
-    </div>
+      <div className="">
+        <div className=" border h-40 w-4/5 md:w-3/5 m-auto flex justify-center items-center">
+          <div className=" h-full w-full relative">
+            <label>
+              <span>
+                <img
+                  className=" h-full w-full -z-10  cursor-pointer "
+                  src={img}
+                  alt="pic"
+                />
+                <Input
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={onImageChange}
+                />
+              </span>
+            </label>
+          </div>
+        </div>
+
+        <div className=" w-full flex justify-center mt-4 mb-20">
+          <Button>
+            <span>Add Banner</span>
+          </Button>
+        </div>
+      </div>
+
+      {Images.map((e) => (
+        <div className="mt-10 sm:mt-10 block w-full sm:w-4/5 sm:m-auto sm:flex sm:items-center ">
+          <div className="w-full border-2px border-black sm:w-11/12 md:w-4/5">
+            <img className="w-full h-full" src={e} />
+          </div>
+          <div className="sm:mb-3 w-full sm:w-1/4 flex justify-center mt-2 ">
+            <Button>delete</Button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
---
+++ src/utils/images/Images.js
@@ -0,0 +1,8 @@
+const Images = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBXWiJk_8-7nHFH3q3L4reHdP-741TbvaZfA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVgbN8sw77WoeHjnfn31dWSiA2WPWO459BnA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYfXeEXMUn9PPZKN7cbCMMDLxwGzGhTreuFg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN7BS0OcxS02X5lIutLO5TX1wr_V8DQQfekg&usqp=CAU",
+];
+
+export default Images;
